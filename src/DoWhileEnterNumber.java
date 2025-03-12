bd9ee297f43f1943c4c5137478acdfbc80e25781
Soe Thant Htut <71380840+SOE13@users.noreply.github.com>
2025-03-12 11:36:40 +0630

    Create DoWhileEnterNumber.java

---
+++ src/DoWhileEnterNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        do {
+            System.out.print("Enter a positive number: ");
+            num = scanner.nextInt();
+        } while (num <= 0);
+        System.out.println("You entered: " + num);
+    }
+}
