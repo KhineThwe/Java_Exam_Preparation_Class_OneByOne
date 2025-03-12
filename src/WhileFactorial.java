a2121821de28bde7a91eec69ec09c11fbec4cf25
Soe Thant Htut <71380840+SOE13@users.noreply.github.com>
2025-03-12 11:33:49 +0630

    Create WhileFactorial.java

---
+++ src/WhileFactorial.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 5, fact = 1;
+        while (num > 0) {
+            fact *= num;
+            num--;
+        }
+        System.out.println("Factorial: " + fact);
+    }
+}
