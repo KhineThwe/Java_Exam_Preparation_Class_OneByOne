ff78d2e0c18ef74a79a1f8c94cd3a717d2681b19
Soe Thant Htut <71380840+SOE13@users.noreply.github.com>
2025-03-12 10:51:25 +0630

    Create NestedIf.java

---
+++ src/NestedIf.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean hasID = true;
+        
+        if (age >= 18) {
+            if (hasID) {
+                System.out.println("You can enter.");
+            } else {
+                System.out.println("ID required.");
+            }
+        } else {
+            System.out.println("You are underage.");
+        }
+    }
+}
