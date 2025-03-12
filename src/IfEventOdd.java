76c2c2e9dc2c9a99e4a260911252a77891c51da4
Soe Thant Htut <71380840+SOE13@users.noreply.github.com>
2025-03-12 11:15:55 +0630

    Create IfEventOdd.java

---
+++ src/IfEventOdd.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 15;
+        if (num > 0) {
+            if (num % 2 == 0) {
+                System.out.println("Positive Even");
+            } else {
+                System.out.println("Positive Odd");
+            }
+        } else {
+            System.out.println("Negative number");
+        }
+    }
+}
